f2f0d0afd68fe8df9a8341c59aeff60319960458
svenrisse <svenrisse0@gmail.com>
2022-04-16 19:46:40 +0200

    Added functionality to add numbers to display when pressed

--- script.js
+++ script.js
@@ -33,5 +33,48 @@ function operate(operator, a, b) {
     }
 }
 
+/* Adding numbers pressed to display */
+let displayValue;
 
-alert(operate("*",5,10))
+function addToDisplay(number) {
+    document.getElementById("display").insertAdjacentHTML("beforeend", number);
+    displayValue = document.getElementById("display").textContent;
+}
+
+const one = document.querySelector("#one");
+one.addEventListener("click", () => {
+    addToDisplay("1");
+}) 
+const two = document.querySelector("#two");
+two.addEventListener("click", () => {
+    addToDisplay("2");
+}) 
+const three = document.querySelector("#three");
+three.addEventListener("click", () => {
+    addToDisplay("3");
+}) 
+const four = document.querySelector("#four");
+four.addEventListener("click", () => {
+    addToDisplay("4");
+}) 
+const five = document.querySelector("#five");
+five.addEventListener("click", () => {
+    addToDisplay("5");
+}) 
+const six = document.querySelector("#six");
+six.addEventListener("click", () => {
+    addToDisplay("6");
+})
+const seven = document.querySelector("#seven");
+seven.addEventListener("click", () => {
+    addToDisplay("7");
+})  
+const eight = document.querySelector("#eight");
+eight.addEventListener("click", () => {
+    addToDisplay("8");
+}) 
+const nine = document.querySelector("#nine");
+nine.addEventListener("click", () => {
+    addToDisplay("9");
+}) 
+
