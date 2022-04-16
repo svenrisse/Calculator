5f59f7b5f3916651ae409721ea3e19ce8acc494a
svenrisse <svenrisse0@gmail.com>
2022-04-16 08:35:55 +0200

    Added basic math operator functions

---
+++ script.js
@@ -0,0 +1,25 @@
+/* basic math operators */
+
+function add(a, b) {
+    console.log(a+b);
+}
+
+add(5,4);
+
+function subtract(a, b) {
+    console.log(a-b);
+}
+
+subtract(5,4);
+
+function multiply(a,b) {
+    console.log(a * b);
+}
+
+multiply(2,5);
+
+function divide(a,b) {
+    console.log(a / b);
+}
+
+divide(10,2);
